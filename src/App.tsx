8211684e44d4afc0246b276f520388dfc5b0f97c
ProjetLT <lilian.taillandier@proton.me>
2023-09-07 15:16:49 +0200

    ajout menu de navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import './App.css';
 import { Home } from './screens/Home';
+import { Ressources } from './screens/Ressources';
+import { Routes, Route, NavLink } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Home/>
+
+      <nav>
+        <NavLink to='/'>Accueil</NavLink>
+        <br />
+        <NavLink to='/ressources'>Ressources</NavLink>
+      </nav>
+
+      <Routes>
+        <Route path='/' element={ <Home/> }/>
+        <Route path='/ressources' element={ <Ressources/> }/>
+      </Routes>
     </div>
   );
 }
--- src/screens/Ressources.tsx
+++ src/screens/Ressources.tsx
@@ -2,6 +2,11 @@ export const Ressources = () => {
     return (
         <div>
             <h3>Ressources du site</h3>
+            <ul>
+                <li><a href="https://youtube.com/" target ="blank">Accès à Youtube</a></li>
+                <li><a href="https://github.com/" target ="blank">Accès à GitHub</a></li>
+                <li><a href="https://google.com/" target ="blank">Ouvrir une page google</a></li>
+            </ul>
         </div>
     )
 }
