cce5f8c491ff7c7c7f9f83d6c4df110cc7f2f421
ProjetLT <lilian.taillandier@proton.me>
2023-09-07 14:46:39 +0200

    premiers test de lancement du site de base avec quelques composants

---
+++ src/components/About.tsx
@@ -0,0 +1,8 @@
+export const About = () => {
+    return (
+        <section>
+            <h1>Bienvenue sur le site</h1>
+            <p>Ceci est un site de test en react js</p>
+        </section>
+    )
+}
---
+++ src/screens/Home.tsx
@@ -0,0 +1,12 @@
+import { About } from "../components/About"
+import { Video } from "../components/Videos"
+
+export const Home = () => {
+    return (
+        <div>
+            <About/>
+            <Video id="yHoI0n2VxMU"/>
+            <Video id="OQFUoXCclrA"/>
+        </div>
+    )
+}
