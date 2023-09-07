e80e9d7acef3d9a67eb759f87cbe79daa42681e8
ProjetLT <lilian.taillandier@proton.me>
2023-09-07 22:22:17 +0200

    changement code html et ajout contact

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { Home } from './screens/Home';
 import { Ressources } from './screens/Ressources';
+import { Contact } from './screens/Contact';
 import { Routes, Route, NavLink } from 'react-router-dom';
 
 function App() {
@@ -11,11 +12,14 @@ function App() {
         <NavLink to='/'>Accueil</NavLink>
         <br />
         <NavLink to='/ressources'>Ressources</NavLink>
+        <br />
+        <NavLink to='/contact'>Contact</NavLink>
       </nav>
 
       <Routes>
         <Route path='/' element={ <Home/> }/>
         <Route path='/ressources' element={ <Ressources/> }/>
+        <Route path='/contact' element={ <Contact/> }/>
       </Routes>
     </div>
   );
---
+++ src/screens/Contact.tsx
@@ -0,0 +1,8 @@
+export const Contact = () => {
+    return (
+        <div>
+            <h3>Contact</h3>
+            <p>Email : testReactJS@mail.fr</p>
+        </div>
+    )
+}
