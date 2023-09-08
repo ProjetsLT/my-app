5adbadd18a4ccbe5c0e2dd4590285ab96a701ea6
ProjetLT <lilian.taillandier@proton.me>
2023-09-08 18:31:39 +0200

    test

--- src/screens/Contact.tsx
+++ src/screens/Contact.tsx
@@ -3,6 +3,18 @@ export const Contact = () => {
         <main>
             <h3 className="text-2xl font-extrabold">Contact</h3>
             <p className="mt-5">Email : testReactJS@mail.fr</p>
+
+            <form className="mt-5">
+                <label className="block" htmlFor="sujet">Sujet de votre mail</label>
+                <input className="border" type="text" name="sujet" id="sujet" placeholder="Quel est le sujet ?"/>
+
+                <label className="block" htmlFor="email">Email</label>
+                <input className="border" type="email" name="email" id="email" placeholder="Entrez votre email"/>
+
+                <label className="block" htmlFor="message">Sujet de votre mail</label>
+                <textarea rows={10} className="border resize-none h-36 w-96" name="message" id="message" placeholder="Entrer votre message"></textarea>
+            </form>
+
         </main>
     )
 }
--- src/screens/Ressources.tsx
+++ src/screens/Ressources.tsx
@@ -1,11 +1,13 @@
+import { BsLink } from 'react-icons/bs'
+
 export const Ressources = () => {
     return (
         <main>
             <h3 className="text-2xl font-extrabold">Ressources du site</h3>
             <ul className="mt-5">
-                <li><a className="hover:underline" href="https://youtube.com/" target ="blank">Accès à Youtube</a></li>
-                <li><a className="hover:underline" href="https://github.com/" target ="blank">Accès à GitHub</a></li>
-                <li><a className="hover:underline" href="https://google.com/" target ="blank">Ouvrir une page google</a></li>
+                <li className='flex items-center'><BsLink className='mr-2'/><a className="hover:underline" href="https://youtube.com/" target ="blank">Accès à Youtube</a></li>
+                <li className='flex items-center'><BsLink className='mr-2'/><a className="hover:underline" href="https://github.com/" target ="blank">Accès à GitHub</a></li>
+                <li className='flex items-center'><BsLink className='mr-2'/><a className="hover:underline" href="https://google.com/" target ="blank">Ouvrir une page google</a></li>
             </ul>
         </main>
     )
