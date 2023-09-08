9c885909407983e7c8466be72d7bd35d5b94b849
ProjetLT <lilian.taillandier@proton.me>
2023-09-08 14:18:10 +0200

    test

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -1,11 +1,13 @@
+import { FaFacebook, FaInstagram, FaTiktok } from 'react-icons/fa';
+
 export const Social = () => {
     return (
         <section className="mt-5">
             <h3 className="text-2xl font-extrabold">Mes réseaux sociaux</h3>
             <ul className="mt-5">
-                <li><a className="hover:text-[blue]" href="https://www.facebook.com/?locale=fr_FR" target ="blank">Facebook</a></li>
-                <li><a className="hover:text-[blue]" href="https://www.instagram.com/ " target ="blank">Instagram</a></li>
-                <li><a className="hover:text-[blue]" href="https://www.tiktok.com/fr/" target ="blank">Tik Tok</a></li>
+                <li className='flex items-center'><FaFacebook className='mr-2'/><a className="hover:text-[blue]" href="https://www.facebook.com/?locale=fr_FR" target ="blank">Facebook</a></li>
+                <li className='flex items-center'><FaInstagram className='mr-2'/><a className="hover:text-[blue]" href="https://www.instagram.com/ " target ="blank">Instagram</a></li>
+                <li className='flex items-center'><FaTiktok className='mr-2'/><a className="hover:text-[blue]" href="https://www.tiktok.com/fr/" target ="blank">Tik Tok</a></li>
             </ul>
         </section>
     )
